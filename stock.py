2f2439d3563ce9e948784db719db3d8df903c489
PANKRAV <96988405+PANKRAV@users.noreply.github.com>
2025-05-30 13:00:00 +0300

    lamprou

--- stock.py
+++ stock.py
@@ -10,11 +10,18 @@ class Stock():
     def __init__(self, name : str, days : int, step : int) :
         self.name = name
         self.stock = yf.download(name, period=f"{days}d", interval=f"{step}m")
+        self.file = f"{self.name}_5d_30m.csv"
+
+
+    def __str__(self):
+        return (str(self.stock))
 
         
 
 def main():
-    pass
+    bmw = Stock("BMW.DE", 5, 30)
+    print(bmw)
+
 
 
 
