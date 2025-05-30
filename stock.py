33128497c15651a0a4648c3016dc4aa6ea4693c4
PANKRAV <96988405+PANKRAV@users.noreply.github.com>
2025-05-30 15:32:05 +0300

    kampyli

--- stock.py
+++ stock.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import numpy as np
 import json
 import scipy
-import matplotlib as mat
+import matplotlib.pyplot as plt
 import csv
 import sys
 from sklearn.metrics import mean_absolute_error, mean_squared_error
@@ -21,6 +21,9 @@ class Stock():
 
     def __str__(self):
         return (str(self.stock.head()))
+
+    def predict(self):
+        ...
     
 
 
@@ -38,7 +41,7 @@ class Model():
         self.errors = []
         self.models= {}
         print(len(self.x_train)/len(self.x_test))
-    
+
 
     def fit(self, deg: int) -> None :
         coeffs = np.polyfit(self.x_train, self.y_train, deg)
@@ -47,8 +50,31 @@ class Model():
 
         mae = mean_absolute_error(self.y_test, y_pred)
         mse = mean_squared_error(self.y_test, y_pred)
-        self.errors.append((deg, mae, mse))
+        self.errors.append((deg, mae, mse, coeffs))
         self.models[deg] = model
+    
+
+    def predict(self, model, N) :
+        len_x = len(self.x)
+        model = np.poly1d(model)
+        prediction_x = np.arange(len_x, len_x + N)
+        prediction_y = model(prediction_x)
+        print(prediction_y)
+        x_fit = np.concatenate([self.x, prediction_y])
+        y_fit = model(x_fit)
+
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.x, self.y, label="Πραγματικά Δεδομένα")
+        plt.plot(x_fit, y_fit, label="Πολυωνυμική Καμπύλη")
+        plt.plot(prediction_x, prediction_y, 'r--', label="Πρόβλεψη 24 ωρών (48 βήματα)")
+        plt.xlabel("Χρονική στιγμή (30 λεπτά)")
+        plt.ylabel("Τιμή μετοχής")
+        plt.title("Πρόβλεψη επόμενης ημέρας")
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
+
         
         
 
@@ -59,6 +85,14 @@ def main():
     #print(bmw)
     for deg in range(1, 4):
         bmw.model.fit(deg)
+
+   #bmw.model.predict(bmw.model.errors[2][3], 48) καμπυλη πολυωνυμου δευτερου βαθμου
+
+    best_model = min(bmw.model.errors, key=lambda t: t[1])
+    bmw.model.predict(best_model[3], 48)
+    
+        
+            
     print(bmw.model.errors)
     
     
